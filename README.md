f264a310623d68194736467774dbdcdc1593b0cb
CHANDRA-BHUSHAN-SAH <chandrabhushan.sah@live.in>
2015-01-25 19:33:55 +0530

    Saving content of README.md file

---
+++ README.md
@@ -0,0 +1,26 @@
+## Peer Assessments /Getting and Cleaning Data Course Project
+
+### Following are the Steps of Problem
+	1. Merges the training and the test sets to create one data set.
+	2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+	3. Uses descriptive activity names to name the activities in the data set
+	4. Appropriately labels the data set with descriptive variable names. 
+	5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+	
+#### Note:
+	* You should have dataset in a folder named "UCI HAR Dataset" in your current working directory
+	* You can download the dataset from the following link and extract it your PWD
+		(https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+	* Download the code file named "run_analysis.R" in your PWD
+  * The RStudio should installed the packages named "plyr" and "reshape2"
+
+### Use the following command to execute the codes of file "run_analysis.R"
+  **source("run_analysis.R")**
+  
+### The following files will be generated as the result of the source code file "run_analysis.R"
+  * tidy.txt
+  * tidy1.txt
+  
+#### The file "tidy.txt" contains the result of the step 5 which is the final result
+
+#### The file "tidy1.txt" contains the intermediate resut through step 1 to step 4 that is the merged data set
---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+## step 1: Merges the training and the test sets to create one data set.
+## (1.1) reading all the data of test and training sets in different objects
+tst_labels <- read.table("UCI HAR Dataset/test/y_test.txt", col.names="label")
+tst_subjects <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names="subject")
+tst_data <- read.table("UCI HAR Dataset/test/X_test.txt")
+trn_labels <- read.table("UCI HAR Dataset/train/y_train.txt", col.names="label")
+trn_subjects <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names="subject")
+trn_data <- read.table("UCI HAR Dataset/train/X_train.txt")
+
+## (1.2) mearging all the data of training and test sets
+data <- rbind(cbind(tst_subjects, tst_labels, tst_data),
+              cbind(trn_subjects, trn_labels, trn_data))
+
+## step 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+## (2.1) reading the features for data set
+features <- read.table("UCI HAR Dataset/features.txt", strip.white=TRUE, stringsAsFactors=FALSE)
+
+## (2.2) extracting features of mean and standard deviation
+features_mean_stddiv <- features[grep("mean\\(\\)|std\\(\\)", features$V2), ]
+
+## (2.3) Extracting only the measurements on the mean and standard deviation for each measurement.
+tidy1 <- data[, c(1, 2, features_mean_stddiv$V1+2)]
+
+## step 3: Uses descriptive activity names to name the activities in the data set
+## (3.1) reading the labels of activities
+labels_activity <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
+
+## (3.2) replacing labels in data with the label names
+tidy1$label <- labels[tidy1$label, 2]
+
+## step 4: Appropriately labels the data set with descriptive variable names. 
+valid_col <- c("subject", "label", features_mean_stddiv$V2)
+valid_col <- tolower(gsub("[^[:alpha:]]", "", valid_col))
+colnames(tidy1) <- valid_col
+
+## step 5:From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+library(plyr)
+library(reshape2)
+tidy2 <- ddply(melt(tidy1, id.vars=c("subject", "label")), .(subject, label), summarise, Average=mean(value))
+names(tidy2) <- c("Subject","Activity","Average_Var_Subject")
+
+## writing the tidy data in files for upload
+write.table(tidy2, "tidy.txt", row.names=F,quote=T)
+write.table(tidy1, "tidy1.txt", row.names=F,quote=T)
